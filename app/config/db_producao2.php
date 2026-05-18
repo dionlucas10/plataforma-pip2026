54aaf95d95607ad168d8e3f2e496c12d6b505082
danisilvafrontend <danisilva.frontend@gmail.com>
2026-05-18 10:49:43 -0300

    Atualização login unificado

---
+++ app/config/db_producao2.php
@@ -0,0 +1,10 @@
+<?php
+// /app/config/db.php
+return [
+    'host' => '127.0.0.1',          
+    'dbname' => 'pipnewdb',
+    'user' => 'pipnewdb',
+    'pass' => 'l6s7v6bgWOAXh6sW73xI',
+    'port' => 3306,
+    'charset' => 'utf8mb4'
+];
